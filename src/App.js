f3425f8334b2e4541f6c213968e6d0bbc0e57ece
Ayushman Saxena <86063191+ayushmansxn01@users.noreply.github.com>
2022-03-06 12:02:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// to start:  npm start
+
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+   
+
+<Navbar title="MyTestUtils" abt="chick"></Navbar>
+    </>
+  );
+}
+
+export default App;
